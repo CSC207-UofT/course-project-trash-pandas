0089313673e6133dc954533847753757efd47788
logatsang <tim.logatsang@gmail.com>
2021-10-15 19:56:35 -0400

    Added constructor to FetchQuest (oops!)

--- src/main/java/FetchQuest.java
+++ src/main/java/FetchQuest.java
@@ -4,6 +4,10 @@ public class FetchQuest extends Quest {
 
     private String goalItem;
 
+    public FetchQuest(String goalItem) {
+        this.goalItem = goalItem;
+    }
+
     public boolean checkItems(Character player) {
         return player.checkItem(this.goalItem);
     }
