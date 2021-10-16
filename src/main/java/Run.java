1af875b2aa3cd1dc013fd140bfb3cd6e9f019162
Eric H <eric.hellebust@mail.utoronto.ca>
2021-10-16 00:30:47 -0400

    Almost completed run class

--- src/main/java/Run.java
+++ src/main/java/Run.java
@@ -19,7 +19,7 @@ public class Run {
                 duringDialogue, endingDialogue, coinQuest);
 
         String streetName = "Street";
-        ArrayList<Character> streetNPCS = new ArrayList<>();
+        ArrayList<NonPlayerCharacter> streetNPCS = new ArrayList<>();
         streetNPCS.add(tim);
         String streetDescription = "You are in the city.";
         ArrayList<String> streetItems = new ArrayList<>();
@@ -28,12 +28,18 @@ public class Run {
 
 
         String pizzaPlaceName = "Pizza Place";
-        ArrayList<Character> pizzaNPCS = new ArrayList<>();
+        ArrayList<NonPlayerCharacter> pizzaNPCS = new ArrayList<>();
         String pizzaPlaceDescription = "This pizza joint is squeaky clean aside from a scrunched up disc" +
                 " of aluminum foil dropped on one of the seats.";
         ArrayList<String> pizzaPlaceItems = new ArrayList<>();
         pizzaPlaceItems.add("coin");
         Scene pizzaPlace = new Scene(pizzaPlaceName, pizzaNPCS, pizzaPlaceDescription, pizzaPlaceItems);
 
+        street.addScene(pizzaPlace);
+        pizzaPlace.addScene(street);
+
+        CommandLine command = new CommandLine();
+
+        command.sceneUI(street, bernie);
     }
 }
