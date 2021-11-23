e8d74ea1c69efa7a06bceb57169663905cdc9510
bernie <bernie.wu@mail.utoronto.ca>
2021-11-22 20:31:47 -0500

    added CombatQuest

---
+++ src/main/java/quest_system/CombatQuest.java
@@ -0,0 +1,30 @@
+package quest_system;
+
+import characters.ItemCheckable;
+import characters.NonPlayerCharacter;
+import items.QuestItem;
+
+import java.util.Set;
+
+public class CombatQuest extends Quest{
+
+    private Set<NonPlayerCharacter> npcs;
+
+    CombatQuest(Set<NonPlayerCharacter> npcs){
+        this.npcs = npcs;
+    }
+
+    public void checkDone() {
+        if(this.npcs.isEmpty() && this.isAccepted()){
+            this.toggleComplete();
+        }
+    }
+
+    @Override
+    public void update(Object args) {
+        if (args != null && this.npcs.contains(((NonPlayerCharacter) args))){
+            this.npcs.remove((NonPlayerCharacter)args);
+        }
+        this.checkDone();
+    }
+}
