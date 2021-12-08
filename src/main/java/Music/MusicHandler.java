543344976ffb3635b8c5d5433f61dbc973f248e4
Wesley <george.lyon@mail.utoronto.ca>
2021-12-08 15:42:02 -0500

    Added java docs to music class

--- src/main/java/Music/MusicHandler.java
+++ src/main/java/Music/MusicHandler.java
@@ -4,6 +4,9 @@ import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 
+/**
+ * Creates a music object that can play music
+ */
 public class MusicHandler {
     Clip clip;
     public void setFile(String soundFileName) {
@@ -18,13 +21,25 @@ public class MusicHandler {
 
         }
     }
+
+    /**
+     * Plays the music one time, useful for dialogue
+     */
     public void play() {
         clip.setFramePosition(0);
         clip.start();
     }
+
+    /**
+     * loops the music
+     */
     public void loop() {
         clip.loop(clip.LOOP_CONTINUOUSLY);
     }
+
+    /**
+     * stops the music
+     */
     public void stop() {
         clip.stop();
         clip.close();
